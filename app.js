6cdc752c198dfac88ba452634eaa16c0ceaaf326
Deirdre Cruice <dmcruice@gmail.com>
2020-11-14 01:15:15 -0500

    progress on app.js

--- app.js
+++ app.js
@@ -10,36 +10,212 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-function addTeamMember() {
+const team = [];
+
+
+function addMember() {
     inquirer.prompt([{
-        type: "input",
-        message: "Enter team member's name",
-        name: "name",
-    },
-    {
+        type: "confirm",
+        message: "Would you like to add a new team member?"
+        default: "y", 
+        name: "addNewMember", 
+    }])
+}.then(function(data){
+    if (data.addNewMember == "y") {
+        teamPrompts();
+    }
+    else {
+        console.log("Your team profile has been generated!")
+    };
+});
+
+function teamPrompts() {
+    inquirer.prompt([{
         type: "list",
         message: "Select team member's role",
         choices: [
-            "Employee",
             "Manager",
             "Engineer",
+            "Employee",
             "Intern",
         ],
         name: "role",
+    }]).then function(data) {
+        let newMember = data.choices;
+
+        if (newMember == "Manager") {
+            newManager();
+        }
+        else if (newMember == "Engineer") {
+            newEngineer();
+        }
+        else if (newMember == "Intern") {
+            newIntern();
+        }
+    }
+}
+
+function newManager() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Enter the Manager's name",
+        name: "name",
     },
     {
         type: "input",
-        message: "Enter team member's phone number",
-        name: "phone",
+        message: "Enter the Manager's office number",
+        name: "office",
     },
     {
         type: "input",
-        message: "Enter team member's email address",
+        message: "Enter the Manager's email address",
         name: "email"
-    }])
-    .then(function({name, role, phone, email}) {
-        
+    },
+]).then((response) => {
+    const manager = new Manager(
+        response.name,
+        response.office,
+        response.email,
+      );
+      team.push(engineer);
+      addMem();
+})
+};
+
+
+
+
+
+
+function buildTeam() {
+    
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(teamArray), "utf-8");
+}
+
+
+
+
+
+
+
+
+function startHtml() {
+    const html = `
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+
+    <body>
+        <div class="col-md-12 jumbotron jumbotron-fluid">
+            <div class="container">
+             <h1 class="display-4 text-center">Team Profile</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row"> 
+    `;
+    fs.writeFile("./output/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
     });
+    console.log("start");
+}
+
+function addHtml(member) {
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        console.log("adding team member");
+        fs.appendFile("./output/team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function endHtml() {
+    const html = ` 
+    </div>
+    </div>
+    </body>
+    `;
+
+    fs.appendFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+
+createProfile();
+
+
+
+
+
+
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
